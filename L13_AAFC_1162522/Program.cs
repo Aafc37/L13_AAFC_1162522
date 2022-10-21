cb8a2b7498efdebd0b666cb0e7ba1c454601e813
aafco <aafco@LAPTOP-9T8JHAEM>
2022-10-21 17:26:38 -0600

    Agregar archivos de proyecto.

---
+++ L13_AAFC_1162522/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bienvenido ");
+        string[] nom = new string[5];
+        int[] ed = new int[5];
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Edad de la persona: ");
+
+            ed[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nombre de la persona: ");
+            nom[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Se muestran la siguientes personas con la mayoria de edad");
+        for (int i = 0; i < 5; i++)
+        {
+            if (ed[i] >= 18)
+            {
+
+                Console.WriteLine(nom[i]);
+
+                Console.WriteLine("y su edad es :" + ed[i]);
+            }
+        }
+        Console.ReadKey();
+    }
+}
